dacedac61c82d6c5b1092b1bd6f002582f21fa80
Luke McShane <lukejsmcshane@gmail.com>
2024-08-28 18:44:16 +0100

    Created initial migration and updated the database with some dummy data

--- API/Data/Migrations/20240828172319_InitialCreate.cs
+++ API/Data/Migrations/20240828172319_InitialCreate.cs
@@ -7,6 +7,10 @@ namespace API.Data.Migrations
     /// <inheritdoc />
     public partial class InitialCreate : Migration
     {
+        /*
+        This method lets us create a table based on the data context and the entity. As you can see the migration understands what the primary key
+        is, that both columns are not able to be null, that the primary key will auto increment, and what the names and data types of the columns are.
+        */
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
