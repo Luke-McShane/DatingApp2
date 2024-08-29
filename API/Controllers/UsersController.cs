2820132c09bd5d8c7a05e1d2c4504445d946c8b3
Luke McShane <lukejsmcshane@gmail.com>
2024-08-29 11:42:22 +0100

    made my code asynchronous and explained the basics of asynchronous code.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using API.Entities;
 using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -12,20 +13,33 @@ namespace API.Controllers;
 public class UsersController(DataContext context) : ControllerBase
 {
   // Within our controller we can now create the API endpoints that the user will use to make API requests.
+  // Made the code asynchronous which enables multiple requests to be handled concurrently.
+  // We make the code asynchronous by wrapping the ActionResult in a task
   [HttpGet]
-  public ActionResult<IEnumerable<AppUser>> GetUsers()
+  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
   {
-    var users = context.Users.ToList();
+    var users = await context.Users.ToListAsync();
     return users;
   }
 
   [HttpGet("{id:int}")] // api/users/1 (1 is an example of an id)
-  public ActionResult<AppUser> GetUser(int id)
+  public async Task<ActionResult<AppUser>> GetUser(int id)
   {
-    var user = context.Users.Find(id);
+    var user = await context.Users.FindAsync(id);
 
     if (user == null) return NotFound();
 
     return user;
   }
 }
+
+/*
+ASYNCHRONOUS PROGRAMMING
+
+- There must be an await keyword in the code body when you use the async keyword to specify the method is asynchronous.
+- All the async keyword does is *enables* the await keyword..nothing more!
+- The async suffix is needed to specify a method is asynchronous.
+- An async method MUST return a Task or Task<T> (or void, but this is rare).
+- A Task is something that runs in the background, freeing the CPU to continue executing code until the task is finished, and then returned. 
+- We return a task because it is awaitable, meaning that it can run asynchronously.
+*/
